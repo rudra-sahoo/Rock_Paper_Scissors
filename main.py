aaa52762423ccd2fc884e0efc2e41b07a4a85bce
Rudra Sahoo <92416423+rudra-sahoo@users.noreply.github.com>
2023-11-03 16:22:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,41 +21,47 @@ Scissors = """
       (____)
 ---.__(___)
 """
+while True:
+    choice_input = int(input("What do you choose Enter 0 for Rock, 1 for Paper, 2 for Scissors"))
+    if choice_input == 0 or choice_input == 1 or choice_input == 2:
+        break
+    else:
+        print("Invalid input please put a valid number")
 choice = [Rock, Paper, Scissors]
-choice_input = int(input("What do you choose Enter 0 for Rock, 1 for Paper, 2 for Scissors"))
 computer_choice = random.randint(0, 2)
 
+
 if choice_input == 0:
-    print("Your choice is", choice[choice_input])
+    print("Your choice is\n", choice[choice_input])
     if computer_choice == 0:
-        print("Computer choice is", choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("Draw")
     elif computer_choice == 1:
-        print("Computer choice is", choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("You Loose")
     else:
-        print("Computer choice is", choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("You won")
 elif choice_input == 1:
-    print("Your choice is", choice[choice_input])
+    print("Your choice is\n", choice[choice_input])
     if computer_choice == 0:
-        print(choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("You won")
     elif computer_choice == 1:
-        print(choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("Draw")
     else:
-        print(choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("You loose")
 else:
-    print("Your choice is", choice[choice_input])
+    print("Your choice is\n", choice[choice_input])
     if computer_choice == 0:
-        print(choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("You loose")
     elif computer_choice == 1:
-        print(choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("You won")
     else:
-        print(choice[computer_choice])
+        print("Computer choice is\n", choice[computer_choice])
         print("Draw")
         
